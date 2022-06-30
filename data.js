056c91050ba7022962031ef96c8475378ec78265
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-06-30 21:20:06 +0430

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
-module.exports = {mongoLink:'mongodb+srv://cluster0.nt676.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',//database linkbot_token:'5541558214:AAGZEyRqXhelFLJJXuo-crxVs3XwsfKtaa8',bot_name:'Tomomomobot',//bot username without @bot_admin:1749574206,channelsList : ['ccggfcg'],channelscheck : ['@ccggfcg'],airdropName : 'Tomo',reffer_bonus:30000,// ral bonus amountmin_wd:10,//min withdrawal amountcurrency:'FMT', //bot cuurencypayment_channel:'@ccggfcg'// payment channel username}
+module.exports = {
+
+mongoLink:'mongodb+srv://cluster0.nt676.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',//database link
+
+bot_token:'5541558214:AAGZEyRqXhelFLJJXuo-crxVs3XwsfKtaa8',
+
+bot_name:'Tomomomobot',//bot username without @
+
+bot_admin:1749574206,
+
+channelsList : ['ccggfcg'],
+
+channelscheck : ['@ccggfcg'],
+
+airdropName : 'Tomo',
+
+reffer_bonus:30000,// ral bonus amount
+
+min_wd:10,//min withdrawal amount
+
+currency:'FMTd', //bot cuurency
+
+payment_channel:'@ccggfcg'// payment channel username
+
+}
